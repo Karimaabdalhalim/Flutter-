0d2759733320470287110ba88a1893734b8f1a49
Karimaabdalhalim <151049912+Karimaabdalhalim@users.noreply.github.com>
2023-12-03 00:00:14 +0200

    Add files via upload

---
+++ task 1.dart
@@ -0,0 +1,7 @@
+void main() {
+  int N = 10; 
+
+  int sum = (N * (N + 1)) ~/ 2;
+
+  print('sum number betwen $N and n is : $sum');
+}
---
+++ task 2.dart
@@ -0,0 +1,9 @@
+void EvenNumbers(int N) {
+  for (int i = 2; i <= N; i += 2) {
+    print(i);
+  }
+}
+void main() {
+  int N = 10; 
+  EvenNumbers(N);
+}
